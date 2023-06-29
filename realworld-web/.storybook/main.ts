70b86b21fe53d11f895d033235b2463d29a8deea
ryutah <6662577+ryutah@users.noreply.github.com>
2023-06-29 18:34:59 +0900

    chore: modify to be able to use import aliases

--- realworld-web/.storybook/main.ts
+++ realworld-web/.storybook/main.ts
@@ -1,4 +1,6 @@
-import type { StorybookConfig } from "@storybook/nextjs";
+import type {StorybookConfig} from "@storybook/nextjs";
+import path from "path";
+
 const config: StorybookConfig = {
   stories: ["../stories/**/*.mdx", "../stories/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
@@ -13,5 +15,12 @@ const config: StorybookConfig = {
   docs: {
     autodocs: "tag",
   },
+  webpackFinal: async (config) => {
+    config.resolve!.alias = {
+      ...config.resolve!.alias,
+      "@": path.resolve(__dirname, "../src"),
+    }
+    return config
+  }
 };
 export default config;
